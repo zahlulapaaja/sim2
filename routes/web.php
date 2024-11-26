05190d94b5fbd41cbd08420663be6c982a3b3dd8
zahlulapaaja <zahlul.2001@gmail.com>
2024-11-26 14:27:01 +0700

    add route resource

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+//route resource for products
+Route::resource('/products', \App\Http\Controllers\ProductController::class);
