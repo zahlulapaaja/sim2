bf00ef5c83d2940b318b8caefa2b176a2e20687c
zahlulapaaja <zahlul.2001@gmail.com>
2024-11-26 15:28:47 +0700

    edit and update success

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -9,10 +9,13 @@ use App\Models\Product;
 use Illuminate\View\View;
 
 //import return type redirectResponse
-use Illuminate\Http\RedirectResponse;
+use Illuminate\Http\Request;
 
 //import Http Request
-use Illuminate\Http\Request;
+use Illuminate\Http\RedirectResponse;
+
+//import Facades Storage
+use Illuminate\Support\Facades\Storage;
 
 class ProductController extends Controller
 {
@@ -87,5 +90,74 @@ class ProductController extends Controller
 
         //render view with product
         return view('products.show', compact('product'));
+    }
+
+    /**
+     * edit
+     *
+     * @param  mixed $id
+     * @return View
+     */
+    public function edit(string $id): View
+    {
+        //get product by ID
+        $product = Product::findOrFail($id);
+
+        //render view with product
+        return view('products.edit', compact('product'));
+    }
+
+    /**
+     * update
+     *
+     * @param  mixed $request
+     * @param  mixed $id
+     * @return RedirectResponse
+     */
+    public function update(Request $request, $id): RedirectResponse
+    {
+        //validate form
+        $request->validate([
+            'image'         => 'image|mimes:jpeg,jpg,png|max:2048',
+            'title'         => 'required|min:5',
+            'description'   => 'required|min:10',
+            'price'         => 'required|numeric',
+            'stock'         => 'required|numeric'
+        ]);
+
+        //get product by ID
+        $product = Product::findOrFail($id);
+
+        //check if image is uploaded
+        if ($request->hasFile('image')) {
+
+            //upload new image
+            $image = $request->file('image');
+            $image->storeAs('products', $image->hashName());
+
+            //delete old image
+            Storage::delete('products/' . $product->image);
+
+            //update product with new image
+            $product->update([
+                'image'         => $image->hashName(),
+                'title'         => $request->title,
+                'description'   => $request->description,
+                'price'         => $request->price,
+                'stock'         => $request->stock
+            ]);
+        } else {
+
+            //update product without image
+            $product->update([
+                'title'         => $request->title,
+                'description'   => $request->description,
+                'price'         => $request->price,
+                'stock'         => $request->stock
+            ]);
+        }
+
+        //redirect to index
+        return redirect()->route('products.index')->with(['success' => 'Data Berhasil Diubah!']);
     }
 }
