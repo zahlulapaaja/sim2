a7ff6ded5748ca3a41633a0c45e2faccb760a360
zahlulapaaja <zahlul.2001@gmail.com>
2024-11-26 14:59:14 +0700

    add show and fix iamge storage

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -73,4 +73,19 @@ class ProductController extends Controller
         //redirect to index
         return redirect()->route('products.index')->with(['success' => 'Data Berhasil Disimpan!']);
     }
+
+    /**
+     * show
+     *
+     * @param  mixed $id
+     * @return View
+     */
+    public function show(string $id): View
+    {
+        //get product by ID
+        $product = Product::findOrFail($id);
+
+        //render view with product
+        return view('products.show', compact('product'));
+    }
 }
