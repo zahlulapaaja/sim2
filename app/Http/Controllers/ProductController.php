5affdb44c62cd1c9d4d4af1641cbea944079339f
zahlulapaaja <zahlul.2001@gmail.com>
2024-11-26 14:36:51 +0700

    insert data success

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -8,6 +8,12 @@ use App\Models\Product;
 //import return type View
 use Illuminate\View\View;
 
+//import return type redirectResponse
+use Illuminate\Http\RedirectResponse;
+
+//import Http Request
+use Illuminate\Http\Request;
+
 class ProductController extends Controller
 {
     /**
@@ -22,5 +28,49 @@ class ProductController extends Controller
 
         //render view with products
         return view('products.index', compact('products'));
+    }
+
+    /**
+     * create
+     *
+     * @return View
+     */
+    public function create(): View
+    {
+        return view('products.create');
+    }
+
+    /**
+     * store
+     *
+     * @param  mixed $request
+     * @return RedirectResponse
+     */
+    public function store(Request $request): RedirectResponse
+    {
+        //validate form
+        $request->validate([
+            'image'         => 'required|image|mimes:jpeg,jpg,png|max:2048',
+            'title'         => 'required|min:5',
+            'description'   => 'required|min:10',
+            'price'         => 'required|numeric',
+            'stock'         => 'required|numeric'
+        ]);
+
+        //upload image
+        $image = $request->file('image');
+        $image->storeAs('public/products', $image->hashName());
+
+        //create product
+        Product::create([
+            'image'         => $image->hashName(),
+            'title'         => $request->title,
+            'description'   => $request->description,
+            'price'         => $request->price,
+            'stock'         => $request->stock
+        ]);
+
+        //redirect to index
+        return redirect()->route('products.index')->with(['success' => 'Data Berhasil Disimpan!']);
     }
 }
